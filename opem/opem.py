7edb3aa2f9a2d1e91ea9cd42892b1c3686f8d148
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-12-15 21:48:29 +0330

    feat : Enerst_calc function added

--- opem/opem.py
+++ opem/opem.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+import math
+def Enerst_Calc(T,PH2,PO2):
+    try:
+        result=1.229-(8.5*(10**-4))*(T-298.15)+(4.308*(10**-5))*T*(math.log(PH2)+0.5*math.log(PO2))
+        return result
+    except Exception:
+        print("[Error] Enerst Calculation Faild")
+
+
+
+
+
+
