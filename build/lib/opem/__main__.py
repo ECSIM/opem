e239703b11b37151a15fc0a5cf3320af58bf73b5
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-12-16 01:43:48 +0330

    feat : art package added

--- build/lib/opem/__main__.py
+++ build/lib/opem/__main__.py
@@ -2,8 +2,10 @@
 
 from .opem import *
 from .params import *
-
+from art import tprint
 if __name__=="__main__":
+    tprint("OPEM")
+    tprint("v"+str(Version))
     Static_Analysis()
 
 
--- opem/params.py
+++ opem/params.py
@@ -6,7 +6,7 @@ m=3*(10**-5)
 xi1=-0.984
 xi3=7.6*(10**-5)
 xi4=-1.93*(10**-4)
-
+Version=0.1
 InputParams={"T":"Cell Operation Temperature [K]","PH2":"Partial Pressure [atm]","PO2":"Partial Pressure [atm]",
            "i":"Cell load current [A]","A":"active area [cm2]","l":"Membrane Thickness [cm]","lambda":"is an adjustable parameter with a possible maximum value of 23",
            "N":"Number Of Single Cells"}
