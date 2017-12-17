7cc12a1af7b1065f3c9ef25717ccb40048805e46
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-12-17 15:59:16 +0330

    fix : testcases fixed

--- opem/test.py
+++ opem/test.py
@@ -20,71 +20,73 @@ False
 >>> lambda_param=23
 >>> N=1
 >>> Enernst_Calc(T,PH2,PO2)
-1.3124275
+1.19075
 >>> CH2_Calc(PH2,T)
-6.242666387198051e-07
+7.330294784824117e-07
 >>> CO2_Calc(PO2,T)
-2.374266952843449e-06
+8.402541445801334e-07
 >>> Rho_Calc(i,A,T,lambda_param)
-371.3457646696301
+4.978789826264977
 >>> Xi2_Calc(A,PH2,T)
-0.002925911881401602
+0.0030373688787134006
 >>> Eta_Conc_Calc(i,A)
-4.4754740929238115e-05
+0
 >>> Eta_Ohmic_Calc(i,l,A,T,lambda_param)
-92.83644116740754
+0
 >>> Eta_Act_Calc(T,PO2,PH2,i,A)
--0.6113210685498855
->>> T=240
->>> PH2=3
->>> PO2=2
->>> i=3.3
->>> A=15
->>> l=2
->>> lambda_param=20
->>> N=20
->>> Enernst_Calc(T,PH2,PO2)
-1.29336956583962
->>> CH2_Calc(PH2,T)
-1.996910545709694e-06
->>> CO2_Calc(PO2,T)
-3.1356482122184143e-06
->>> Rho_Calc(i,A,T,lambda_param)
-29.30233863355657
->>> Xi2_Calc(A,PH2,T)
-0.0028372819403690084
->>> Eta_Conc_Calc(i,A)
-0.0001743731218320777
->>> Eta_Ohmic_Calc(i,l,A,T,lambda_param)
-12.89302899876489
->>> Eta_Act_Calc(T,PO2,PH2,i,A)
--0.5895044084663448
->>> Test_Vector_1={"T":200,"PH2":1,"PO2":1,"i":1.5,"A":30,"l":5,"lambda":5,"N":8}
+0
+>>> Test_Vector_1={"T":343.15,"PH2":1,"PO2":1,"i":0,"A":50.6,"l":0.178,"lambda":23,"N":1}
 >>> Static_Analysis(Test_Vector_1,TestMode=True)
 Analyzing . . .
 Done!
-Enernst : 1.3124275
-Eta Activation : -0.6113210685498855
-Eta Concentration : 4.4754740929238115e-05
-Eta Ohmic : 92.83644116740754
-Loss : 92.22516485359859
-PEM Efficiency : -58.27739573948627
-Power : -136.3691060303979
-VStack : -727.3018988287887
-Vcell : -90.91273735359859
->>> Test_Vector_2={"T":240,"PH2":3,"PO2":2,"i":3.3,"A":15,"l":2,"lambda":20,"N":20}
+Enernst : 1.19075
+Eta Activation : 0
+Eta Concentration : 0
+Eta Ohmic : 0
+Loss : 0
+PEM Efficiency : 0.763301282051282
+Power : 0.0
+VStack : 1.19075
+Vcell : 1.19075
+>>> Test_Vector_2={"T":343.15,"PH2":1,"PO2":1,"i":25.3,"A":50.6,"l":0.178,"lambda":23,"N":1}
 >>> Static_Analysis(Test_Vector_2,TestMode=True)
 Analyzing . . .
 Done!
-Enernst : 1.29336956583962
-Eta Activation : -0.5895044084663448
-Eta Concentration : 0.0001743731218320777
-Eta Ohmic : 12.89302899876489
-Loss : 12.303698963420377
-PEM Efficiency : -7.0579034599876636
-Power : -36.3340870120165
-VStack : -220.20658795161512
-Vcell : -11.010329397580756
+Enernst : 1.19075
+Eta Activation : -0.5205357898108074
+Eta Concentration : 0.0016379445009943273
+Eta Ohmic : 0.48876763670032947
+Loss : -0.030130208609483598
+PEM Efficiency : 0.7826155183394127
+Power : 30.888269277819937
+VStack : 1.2208802086094837
+Vcell : 1.2208802086094837
+>>> Test_Vector_3={"T":343.15,"PH2":1,"PO2":1,"i":50.6,"A":50.6,"l":0.178,"lambda":23,"N":1}
+>>> Static_Analysis(Test_Vector_3,TestMode=True)
+Analyzing . . .
+Done!
+Enernst : 1.19075
+Eta Activation : -0.5664415066275723
+Eta Concentration : 0.08942873961125186
+Eta Ohmic : 1.1356047108209584
+Loss : 0.6585919438046379
+PEM Efficiency : 0.34112695909959106
+Power : 26.92719764348532
+VStack : 0.532158056195362
+Vcell : 0.532158056195362
+>>> Test_Vector_4={"T":343.15,"PH2":1,"PO2":1,"i":75.9,"A":50.6,"l":0.178,"lambda":23,"N":1}
+>>> Static_Analysis(Test_Vector_4,TestMode=True)
+Analyzing . . .
+Done!
+Enernst : 1.19075
+Eta Activation : -0.5932946295341045
+Eta Concentration : 4.882643742570118
+Eta Ohmic : 2.103719891323974
+Loss : 6.393069004359988
+PEM Efficiency : -3.3348198745897353
+Power : -394.85601243092304
+VStack : -5.202319004359987
+Vcell : -5.202319004359987
 >>> T='20000000000'
 >>> PH2='10000000'
 >>> PO2='1000000000'
