7f02bc7861b4ac5a4698f5ad9fff008ed3076c14
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-12-29 01:12:02 +0330

    fix : Larminiee params names fixed

--- opem/Larminiee_Params.py
+++ opem/Larminiee_Params.py
@@ -4,9 +4,9 @@ R1=8.314
 InputParams = {"T": "Cell Operation Temperature [K]", "PH2": "Partial Pressure [atm]", "PO2": "Partial Pressure [atm]",
                "i-start": "Cell operating current start point [A]", "i-step": "Cell operating current step",
                "i-stop": "Cell operating current end point [A]",
-               "N": "Number Of Single Cells", "R": "R-Electronic [ohm] (*Optional)",
-               "alpha": "charge transfer coefficient",
-               "I_N": "maximum current density [A/cm2]",
-               "I_0":"maximum current density [A/cm2]",
-               "I_Lim":"maximum current density [A/cm2]"}
+               "N": "Number Of Single Cells", "R": "Membrane and Contact Resistances [ohm]",
+               "alpha": "Charge Transfer Coefficient",
+               "I_N": "Internal Current [A]",
+               "I_0":"Exchange Current [A]",
+               "I_Lim":"Limiting Current [A]"}
 OutputParams = {"Enernst": "V","Vcell": "V", "PEM Efficiency": "", "Power": "W", "VStack": "V"}
