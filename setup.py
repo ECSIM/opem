717f7ac2ad411a30aae81df036d52f9e70ec00b4
sepandhaghighi <sepand.haghighi@yahoo.com>
2018-01-31 03:08:16 +0330

    version changed

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'opem',
   packages = ['opem'],
-  version = '0.2',
+  version = '0.3',
   description = 'Open Source PEM Cell Simulation Tool',
   long_description='''Open Source PEM Cell Simulation Tool''',
   author = 'Sepand Haghighi,Kasra Askari,Sarmin Hamidi,Mohammad Mahdi Rahimi',
   author_email = 'opem@ecsim.ir',
   url = 'https://github.com/ecsim/opem',
-  download_url = 'https://github.com/ecsim/opem/tarball/v0.2',
+  download_url = 'https://github.com/ecsim/opem/tarball/v0.3',
   keywords = ["OPEM","PEM","FC","CELL","Fuel Cell","Chemistry"],
   install_requires=[
       'art',
