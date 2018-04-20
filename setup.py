c5aad32e2ff369fa57bdd2c911f1977985c1b19e
sepandhaghighi <sepand.haghighi@yahoo.com>
2018-04-20 13:04:59 +0430

    fix : python 3.3 removed from config

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setup(
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
