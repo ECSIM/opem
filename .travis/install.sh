0c4ca4b78c3379bba8fba1da0980ce807f630412
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2018-12-24 14:12:24 +0330

    use pyenv for diffrent versions

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,10 +1,30 @@
+#!/bin/bash
+
 PIP=`which pip || (python --version 2>&1 | grep -q 'Python 2' && which pip2) || (python --version 2>&1 | grep -q 'Python 3' && which pip3)`
 
-# Install Python3 on osx
-if [ "$TRAVIS_OS_NAME" = "osx" ] && [ ! -z "$TOXENV" ]
-then
-  $PIP install --user -U tox
-  brew install python3
-  exit 0
-fi
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
+    brew install pyenv >> /dev/null
+
+    case "${TOXENV}" in
+        py34)
+            pyenv install 3.4.9
+	    pyenv global 3.4.9
+	    ;;
+        py35)
+            pyenv install 3.5.6
+	    pyenv global 3.5.6
+            ;;
+	py36)
+            pyenv install 3.6.7
+	    pyenv global 3.6.7
+	    ;;
+	py37)
+            pyenv install 3.7.1
+	    pyenv global 3.7.1
+	    ;;
+	*)
+	    brew upgrade python
+	    ;;
+    esac
+fi
