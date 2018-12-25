cdb35428550a790db09b4df299668a8efb62b39a
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2018-12-26 02:48:01 +0330

    minor fix

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	pip3 install tox
+    case "${TOXENV}" in
+        py34)
+        pyenv install 3.4.8
+	    pyenv global 3.4.8
+	    ;;
+        py35)
+        pyenv install 3.5.5
+	    pyenv global 3.5.5
+            ;;
+	py36)
+        pyenv install 3.6.5
+	    pyenv global 3.6.5
+	    ;;
+	py37)
+        pyenv install 3.7.0b3
+	    pyenv global 3.7.0b3
+	    ;;
+	*)
+	    brew upgrade python
+	    ;;
+    esac
+
 	exit 0
 fi
 pip install -r requirements.txt
