4a76372f2066b170d9557fdbd5e37287f39fd814
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2018-12-26 02:25:07 +0330

    fix requirements

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,6 +3,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	pip3 install tox
 	exit 0
 fi
-
+pip install -r requirements.txt
+python setup.py install
 # Make sure tox is installed and up to date
 pip install -U tox
