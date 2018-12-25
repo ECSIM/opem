f70bb35bffe8866359b05dc8df6a8e742c0aa8a8
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2018-12-26 01:21:50 +0330

    Update install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,20 +7,20 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     pyenv install -l
     case "${TOXENV}" in
         py34)
-            pyenv install 3.4.9
-	    pyenv global 3.4.9
+            pyenv install 3.4.8
+	    pyenv global 3.4.8
 	    ;;
         py35)
-            pyenv install 3.5.6
-	    pyenv global 3.5.6
+            pyenv install 3.5.5
+	    pyenv global 3.5.5
             ;;
 	py36)
-            pyenv install 3.6.7
-	    pyenv global 3.6.7
+            pyenv install 3.6.5
+	    pyenv global 3.6.5
 	    ;;
 	py37)
-            pyenv install 3.7.1
-	    pyenv global 3.7.1
+            pyenv install 3.7.0b3
+	    pyenv global 3.7.0b3
 	    ;;
 	*)
 	    brew upgrade python
