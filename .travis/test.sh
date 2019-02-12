2339ae94cb5eb7fae08d453de2c4e8d861785967
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-02-12 12:54:51 +0330

    fix : version_check added #93

--- .travis/test.sh
+++ .travis/test.sh
@@ -2,4 +2,5 @@
 # Dump Environment (so that we can check PATH, UT_FLAGS, etc.)
 set
 
+python version_check.py
 tox -- --cov=opem --cov-report=term
---
+++ version_check.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import codecs
+Failed = 0
+VERSION = "0.9"
+
+VERSION_1 = VERSION.split(".")[0]
+VERSION_2 = str(int(float(VERSION)*10 - int(VERSION_1)*10))
+VERSION_3 = str(int(float(VERSION)*100 - int(VERSION_1)*100 - int(VERSION_2)*10))
+VERSION_4 = "0"
+
+SETUP_ITEMS = [
+    "version='{0}'",
+    'https://github.com/ecsim/opem/tarball/v{0}']
+INSTALL_ITEMS = [
+    "[Version {0}](https://github.com/ecsim/opem/archive/v{0}.zip)",
+    "[Exe-Version {0}]",
+    "[DMG-Version {0}]"]
+CHANGELOG_ITEMS = [
+    "## [{0}]",
+    "https://github.com/ECSIM/opem/compare/v{0}...HEAD",
+    "[{0}]:"]
+DOCUMENT_ITEMS = [
+    "### Version {0}"]
+
+HTML_ITEMS = ["Version {0}"]
+PARAMS_ITEMS = ["Version = {0}"]
+RC_ITEMS =["filevers=({0}, {1}, {2}, {3})","prodvers=({0}, {1}, {2}, {3})","(u'FileVersion', u'{0}.{1}.{2}.{3}'),","(u'ProductVersion', u'{0}, {1}, {2}, {3}')"]
+FILES = {
+    "setup.py": SETUP_ITEMS, "INSTALL.md": INSTALL_ITEMS, "CHANGELOG.md": CHANGELOG_ITEMS, os.path.join(
+        "Documents", "Amphlett.ipynb"): DOCUMENT_ITEMS,os.path.join(
+        "Documents", "Chamberline_Kim.ipynb"): DOCUMENT_ITEMS,os.path.join(
+        "Documents", "Larminie_Dicks.ipynb"): DOCUMENT_ITEMS,os.path.join(
+        "Documents", "Padulles_Amphlett.ipynb"): DOCUMENT_ITEMS,os.path.join(
+        "Documents", "Padulles_Hauer.ipynb"): DOCUMENT_ITEMS,os.path.join(
+        "Documents", "Padulles1.ipynb"): DOCUMENT_ITEMS,os.path.join(
+        "Documents", "Padulles2.ipynb"): DOCUMENT_ITEMS, os.path.join(
+        "otherfile", "test.html"): HTML_ITEMS,os.path.join("opem", "Params.py"): PARAMS_ITEMS}
+
+TEST_NUMBER = len(FILES.keys()) +1
+
+
+def print_result(failed=False):
+    message = "Version tag tests "
+    if not failed:
+        print("\n" + message + "passed!")
+    else:
+        print("\n" + message + "failed!")
+    print("Passed : " + str(TEST_NUMBER - Failed) + "/" + str(TEST_NUMBER))
+
+
+if __name__ == "__main__":
+    for file_name in FILES.keys():
+        try:
+            file_content = codecs.open(
+                file_name, "r", "utf-8", 'ignore').read()
+            for test_item in FILES[file_name]:
+                if file_content.find(test_item.format(VERSION)) == -1:
+                    print("Incorrect version tag in " + file_name)
+                    Failed += 1
+                    break
+        except Exception as e:
+            Failed += 1
+            print("Error in " + file_name + "\n" + "Message : " + str(e))
+    try:
+        file_content = codecs.open(os.path.join("otherfile","Version.rc"), "r", "utf-8", 'ignore').read()
+        for test_item in RC_ITEMS:
+            if file_content.find(test_item.format(VERSION_1,VERSION_2,VERSION_3,VERSION_4)) == -1:
+                print("Incorrect version tag in " + "Version.rc")
+                Failed += 1
+                break
+    except Exception as e:
+        Failed += 1
+        print("Error in " + file_name + "\n" + "Message : " + str(e))
+    if Failed == 0:
+        print_result(False)
+        sys.exit(0)
+    else:
+        print_result(True)
+        sys.exit(1)
