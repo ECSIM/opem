6be0a1715523fa4c796c4465e9afd109fba2b6e2
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-02-12 13:04:26 +0330

    fix : vulture test added

--- .travis/test.sh
+++ .travis/test.sh
@@ -4,5 +4,6 @@ set
 
 python version_check.py
 python -m bandit -r opem -s B322
+python -m vulture --min-confidence 80 --exclude=opem,build,.eggs --sort-by-size .
 tox -- --cov=opem --cov-report=term
 python -m cProfile -s cumtime opem/Profile.py
