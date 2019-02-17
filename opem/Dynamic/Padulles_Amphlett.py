dbea6b2f6dc1129aa2807062b15c402c4e3d384e
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-02-18 01:19:04 +0330

    fix : Padulles-Amphlett model Eta return added

--- opem/Dynamic/Padulles_Amphlett.py
+++ opem/Dynamic/Padulles_Amphlett.py
@@ -331,7 +331,10 @@ def Dynamic_Analysis(
                 "PH2O": PH2O_List,
                 "Ph": Power_Thermal_List,
                 "V0": B0,
-                "K": B1}
+                "K": B1,
+                "Eta_Active": Eta_Active_List,
+                "Eta_Conc": Eta_Conc_List,
+                "Eta_Ohmic": Eta_Ohmic_List}
     except Exception:
         if TestMode:
             return {
