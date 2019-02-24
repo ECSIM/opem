2a7e25a955e864d13e2f55c0a3dc2bdf6546f46c
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-02-24 19:24:11 +0330

    fix : minor edit in setup.py classifiers

--- setup.py
+++ setup.py
@@ -62,7 +62,9 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Physics',
     ],
     license='MIT',
 )
