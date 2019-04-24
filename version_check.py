2858e31e6f01096539d07b26290b6475db2d78ce
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-04-25 01:23:11 +0430

    doc : version_check.py docstring modified #108

--- version_check.py
+++ version_check.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Version-check script."""
 import os
 import sys
 import codecs
@@ -44,6 +45,13 @@ TEST_NUMBER = len(FILES.keys()) +1
 
 
 def print_result(failed=False):
+    """
+    Print final result.
+
+    :param failed: failed flag
+    :type failed: bool
+    :return: None
+    """
     message = "Version tag tests "
     if not failed:
         print("\n" + message + "passed!")
