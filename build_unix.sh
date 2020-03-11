84e0213b783dd08dfaf55a6bc74a8b280fd80036
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-03-11 16:03:10 +0330

    fix : minor edit in build_unix.sh

--- build_unix.sh
+++ build_unix.sh
@@ -12,7 +12,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         pyinstaller -y --clean --windowed OPEM.spec
 	pushd dist
-	hdiutil create ./Opem.dmg -srcfolder OPEM -ov
+	hdiutil create ./OPEM.dmg -srcfolder OPEM -ov
 	popd
 elif [[ "$OSTYPE" == "cygwin" ]]; then
         echo "You are usign $OSTYPE, that means you are using windows OS, so please run build_exe.bat instead."
