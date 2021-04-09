acd636f0d56fedf7d45137838646165f1219bdff
sepandhaghighi <sepand.haghighi@yahoo.com>
2021-04-10 01:19:04 +0430

    fix : autopep8 scripts added

---
+++ autopep8.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python -m autopep8 opem --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
+python -m autopep8 setup.py --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
+python -m autopep8 otherfile --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
