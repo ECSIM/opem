96ff3fd3c47e3301847d341929b99d660bdce4a9
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-05-13 01:33:16 +0330

    fix : Python 3.6 support dropped

--- build_exe.bat
+++ build_exe.bat
@@ -1,8 +1,8 @@
 @echo off
-python --version | find /i "Python 3.4" > nul
+python --version | find /i "Python 3.7" > nul
 echo -----
 if errorlevel 1 (
-	echo Warning : Please use Python 3.4.x
+	echo Warning : Please use Python 3.7.x
 ) else (
 	echo Python version check done!
 )
